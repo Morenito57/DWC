b1cf49b2a11d4e31c8f50435b7b19a58d40a4288
Rus_Yeray <yerayrusmartinez@gmail.com>
2022-10-04 19:10:13 +0200

    4/10/22

--- js/buscaminas.js
+++ js/buscaminas.js
@@ -20,13 +20,32 @@ document.write('</table>');
 // Crear array bidimensual para guardar las minas
 
 let arrayTablero =[];
+let contadorMinas = 0;
+let posFila;
+let posColumna;
 
-for (let mina = 0; mina < numMinas; mina++) {
-    posFila = Math.floor(Math.random()*maxFilas);
-    console.log(posFila);
-    //arrayTablero[posFila][posColumna] = 'MINA';
+for (let fila = 0; fila < maxFilas; fila++) {
+    arrayTablero[fila] = [];
+
+    for (let columna = 0; columna < maxColumnas; columna++) {
+        arrayTablero[fila][columna] = '';
+    }
 }
+
+while (contadorMinas < numMinas) {
+        posFila = Math.floor(Math.random()*maxFilas);
+        posColumna = Math.floor(Math.random()*maxColumnas);
+        
+        if (arrayTablero[posFila][posColumna] != 'MINA') {
+            arrayTablero[posFila][posColumna] = 'MINA';
+            contadorMinas++ ;
+        };
+}
+
+console.log(contadorMinas);
 console.log(arrayTablero);
+
+
 
 /*<tr>
       
