50c3030a34e8364f5289de660a83b2a22823146d
Rus_Yeray <yerayrusmartinez@gmail.com>
2022-10-21 16:37:42 +0200

    Termine de crear el aviso de las dimensiones tabla

--- JuegoMemoria_RusYeray/js/juegoMemoria.js
+++ JuegoMemoria_RusYeray/js/juegoMemoria.js
@@ -1,3 +1,11 @@
-let maxFilas = prompt('¿Cuántas filas quieres en la tabla?');
-let maxColumnas = prompt('¿Cuántas columnas quieres en la tabla?');
+adelante = true;
+while (adelante){
+    let maxFilas = prompt('¿Cuántas filas quieres en la tabla?');
+    let maxColumnas = prompt('¿Cuántas columnas quieres en la tabla?');
 
+    if((maxFilas * maxColumnas) % 2 == 0){
+        adelante = false;
+    }else{
+        alert('El numero de casillas de la tabla es impar, introduzca otra vez las dimensiones de la tabla');
+    }
+}
